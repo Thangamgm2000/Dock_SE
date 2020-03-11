0273b5e7c22f369ce1ccbc6be15601d0a0342c3e
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-03-11 19:55:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ set -e
 echo "Building image..."
 aws --version
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 122927489672.dkr.ecr.us-east-1.amazonaws.com/devops
-docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:ver$(date "+%s") .
+tym=$(date "+%s")
+docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:ver$tym .
 echo "Pushing image"
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:ver$(date "+%s")
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:ver$tym
 echo "Updating CFN"
 aws cloudformation update-stack --stack-name $STACK_NAME --use-previous-template --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=DockerImageURL,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:ver$(date "+%s") \
+  --parameters ParameterKey=DockerImageURL,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:ver$tym  \
   ParameterKey=DesiredCapacity,UsePreviousValue=true \
   ParameterKey=InstanceType,UsePreviousValue=true \
   ParameterKey=MaxSize,UsePreviousValue=true \
