b1667774dd4053c9199de84ff1d34412366ea92d
THANGA MANICKAM M <thanga.manickam5@gmail.com>
2020-03-15 21:08:16 +0530

    Updated test cases

--- spec/serverspec.js
+++ spec/serverspec.js
@@ -1,16 +1,50 @@
 var request = require('request');
 
 it("should return as Game Theory", function(done) {
-  request("http://localhost:3090/hello", function(error, response, body){
+  request("https://kxt4vjniid.execute-api.us-east-1.amazonaws.com/prod/seatsfetch", function(error, response, body){
     //console.log(response);
-    expect(body).toEqual("Game Theory");
+    expect(body).toEqual('{"seats_left":10}');
     done();
   });
 });
 
 it("should not  return null", function(done) {
-    request("http://localhost:3090/adminfetch", function(error, response, body){
+    request("https://kxt4vjniid.execute-api.us-east-1.amazonaws.com/prod/adminfetch", function(error, response, body){
       //console.log(response);
+      expect(body).not.toBe(null);
+      done();
+    });
+  });
+
+it("should return a valid value", function(done) {
+    request.post("https://kxt4vjniid.execute-api.us-east-1.amazonaws.com/prod/getprefassigned",{json: {roll:'CB.EN.U4CSE17101'}}, function(error, response, body){
+      //console.log(response);
+	  //console.log(body)
+      expect(body).not.toBe(null);
+      done();
+    });
+  });
+  //---------------------------------------------------------------
+  it("correct password should not be null", function(done) {
+    request.post("https://kxt4vjniid.execute-api.us-east-1.amazonaws.com/prod/verify",{json: {roll:'CB.EN.U4CSE17101',pass:'password'}}, function(error, response, body){
+      //console.log(response);
+	  //console.log(body)
+      expect(body['name']).not.toBe(' ');
+      done();
+    });
+  });
+  it("invalid password should return empty", function(done) {
+    request.post("https://kxt4vjniid.execute-api.us-east-1.amazonaws.com/prod/verify",{json: {roll:'CB.EN.U4CSE17101',pass:'password1'}}, function(error, response, body){
+      //console.log(response);
+	  //console.log(body)
+      expect(body['name']).toBe('');
+      done();
+    });
+  });
+  it("Working of server", function(done) {
+    request.get("https://kxt4vjniid.execute-api.us-east-1.amazonaws.com/prod/", function(error, response, body){
+      //console.log(response);
+	  //console.log(body)
       expect(body).not.toBe(null);
       done();
     });
